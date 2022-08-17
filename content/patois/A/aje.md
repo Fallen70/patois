94e6560b1d3a0d1ad832b36387cbeb39062260ba
Fallen70 <julien.petitperrin@gmail.com>
2022-08-17 10:09:08 +0200

    Initial commit

---
+++ content/patois/A/aje.md
@@ -0,0 +1,5 @@
+Title: Aje
+Date: 2010-12-03 10:20
+Category: patois
+Tags: A
+Trad: Content
---
+++ pelicanconf.py
@@ -0,0 +1,32 @@
+AUTHOR = 'Fallen'
+SITENAME = 'Patois'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Rome'
+
+DEFAULT_LANG = 'fr'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+PATH_METADATA = '(?P<path_no_ext>.*)\..*'
+ARTICLE_URL = ARTICLE_SAVE_AS = PAGE_URL = PAGE_SAVE_AS = '{path_no_ext}.html'
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),)
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
