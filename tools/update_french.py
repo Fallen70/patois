18feb83e16b1c95f2362c40d9b9d84c155b3fbc4
Fallen70 <julien.petitperrin@gmail.com>
2023-08-02 11:40:14 +0200

    More verbose tools

--- tools/update_french.py
+++ tools/update_french.py
@@ -3,16 +3,18 @@
 from word import Word
 import os
 
-path = 'content/patois/'
-words = []
-errors = []
-for directory in os.listdir(path):
-    for file in os.listdir(f"{path}/{directory}"):
-        w = Word.from_file( f"{path}/{directory}/{file}" )
-        results, error = w.to_french()
-        words +=  results 
-        errors += error
-for w in words:
-    w.set_path()
-    w.save_or_update()
+if __name__ == '__main__':
+    path = 'content/patois/'
+    words = []
+    errors = []
+    for directory in os.listdir(path):
+        for file in os.listdir(f"{path}/{directory}"):
+            print( f"Reading {path}/{directory}/{file}" )
+            w = Word.from_file( f"{path}/{directory}/{file}" )
+            results, error = w.to_french()
+            words +=  results 
+            errors += error
+    for w in words:
+        w.set_path()
+        w.save_or_update()
 
